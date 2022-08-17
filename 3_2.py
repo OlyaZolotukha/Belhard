652ad38c54925ce3a8ac46f5987594e8097f0de9
artem <artsiom.malets@gmail.com>
2022-08-17 18:11:02 +0300

    third task complete

--- 3_2.py
+++
@@ -1,6 +0,0 @@
-#Пользователь вводит 3 числа, найти среднее арифмитическое сточность 3
-
-first_number = float(input())
-second_number = float(input())
-third_number = float(input())
-print(round(((first_number + second_number + third_number) / 3), 3))
